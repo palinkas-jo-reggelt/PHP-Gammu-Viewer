04ba6adbb87843093d0a499855fa5ec58918669b
Ad Min <admin@123.dynu.com>
2020-01-25 12:47:24 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php include("head.php") ?>
+
+<!-- REARRANGE PAGES OR MOVE TO NEW PAGE -->
 <?php include("status.php") ?>
 <?php include("outbox.php") ?>
 <?php include("inbox.php") ?>
 <?php include("send.php") ?>
+<!-- REARRANGE PAGES OR MOVE TO NEW PAGE -->
+
 <?php include("foot.php") ?>
