77e4759b2159b6c24faf98970ed29de58244fb7e
palinkas-jo-reggelt <52140617+palinkas-jo-reggelt@users.noreply.github.com>
2020-12-02 18:21:14 -0500

    Update config.dist.php

--- config.dist.php
+++ config.dist.php
@@ -46,7 +46,7 @@ $ServerLocation = array (
 );
 
 /*  TimeZone required to determine if gammu is connected or not - status.php
-	TimeZone offset required to compare last update in database against current time
+    TimeZone offset required to compare last update in database against current time
     https://www.php.net/manual/en/timezones.php
 */
 
