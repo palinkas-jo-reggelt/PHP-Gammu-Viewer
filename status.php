d5cab26a39adeebc3859849dbe3ae0c40fb9b911
palinkas-jo-reggelt <52140617+palinkas-jo-reggelt@users.noreply.github.com>
2020-12-02 18:16:27 -0500

    Update status.php

--- status.php
+++ status.php
@@ -8,7 +8,7 @@
 	$UpdatedInDB = $sql->fetchColumn();
 	$UIDB = strtotime($UpdatedInDB." ".$TimeZone);
 	
-	if((time() - $UIDB) > 90) {
+	if ((time() - $UIDB) > 90) {
 		echo "<b>STATUS</b>: <span style='color:red;font-weight:bold;'>DISCONNECTED</span>";
 	} else {
 		echo "<b>STATUS</b>: <span style='color:green;font-weight:bold;'>CONNECTED</span>";
