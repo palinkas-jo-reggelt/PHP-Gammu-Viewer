fa02d282c21851b9e6aaad29caf22d169cc88d87
palinkas-jo-reggelt <52140617+palinkas-jo-reggelt@users.noreply.github.com>
2021-01-04 11:02:47 -0500

    Update head.php

--- head.php
+++ head.php
@@ -13,7 +13,7 @@
 <!DOCTYPE html> 
 <html>
 <head>
-<title>wap.dynu.net SMS Gateway</title>
+<title>SMS Gateway</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <meta http-equiv="Content-Style-Type" content="text/css">
 <meta name="viewport" content="width=device-width, initial-scale=1">
